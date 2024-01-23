1ca195d34c84d4d6066f965cd550fd0c93cb0cb4
JadeLamer <157183869+JadeLamer@users.noreply.github.com>
2024-01-24 01:45:11 +0800

    Update job_listings.php

--- company/job_listings.php
+++ company/job_listings.php
@@ -127,6 +127,39 @@ try {
 <head>
     <title>Company - Manage Job Listings</title>
     <!-- Include necessary stylesheets or scripts -->
+
+<head>
+    <title>PESO Muntinlupa - Job Portal</title>
+    <link rel="stylesheet" href="css/index.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="icon" type="image/png" href="/img/peso_muntinlupa.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+    <style>
+        .cover {
+            /* use 1350x300px image */
+            background-image: url('img/test_cover2.png');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            height: 300px;
+
+        }
+
+        @media (max-width: 1200px) {
+            .cover {
+                display: none;
+                /* Hide the cover image on devices with resolution 1200px and below */
+            }
+        }
+    </style>
+</head>
+
 </head>
 
 <body>
