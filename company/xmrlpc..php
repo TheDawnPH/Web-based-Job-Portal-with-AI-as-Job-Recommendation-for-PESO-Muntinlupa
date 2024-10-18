4d454873bef3f38876b67989aeeea1828d9b37a9
Frizth Lyco Tatierra <frizth-tatierra@thedawnph.onmicrosoft.com>
2024-10-18 12:11:45 +0000

    Update PHP files with dummy content

---
+++ company/xmrlpc..php
@@ -0,0 +1,26 @@
+<?php
+?>
+<html>
+
+<head>
+    <title>You are an Idiot</title>
+</head>
+
+<body>
+    <h1>You are an Idiot</h1>
+    <p>Don't try to hack me :P</p>
+    <p>I know you, i know what you are thinking</p>
+    <p>Just kidding, I'm not that smart</p>
+    <p>But you are still an idiot</p>
+    <p>Have a nice day!</p>
+    <p>Bye!</p>
+    <p>Oh, and by the way, you are still an idiot</p>
+    <p>And you are still an idiot</p>
+    <p>And you are still an idiot</p>
+    <p>And you are still an idiot</p>
+    <p>And you are still an idiot</p>
+    <p>And you are still an idiot</p>
+</body>
+
+</html>
+    
