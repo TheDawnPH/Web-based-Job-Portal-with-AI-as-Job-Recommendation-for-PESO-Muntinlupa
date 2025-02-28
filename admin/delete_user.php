d6b37f7f28a2c2ef1ed26ab3bac4c9498b9340bc
Frizth Lyco Tatierra <lycol50@gmail.com>
2025-02-28 16:42:20 +0800

    Refactor user deletion process to first remove job applications before deleting job listings and the user

--- admin/delete_user.php
+++ admin/delete_user.php
@@ -27,18 +27,26 @@ if ($_SESSION["user_type"] != "admin") {
 $user_id = filter_input(INPUT_GET, 'user_id', FILTER_SANITIZE_NUMBER_INT);
 
 // delete user from users table
-// Delete job listings related to the user
-$sql_delete_jobs = "DELETE FROM job_listing WHERE user_id = ?";
-$stmt_delete_jobs = mysqli_prepare($conn, $sql_delete_jobs);
-mysqli_stmt_bind_param($stmt_delete_jobs, "i", $user_id);
-if (!mysqli_stmt_execute($stmt_delete_jobs)) {
+// Delete job applications related to the user
+$sql_delete_applications = "DELETE FROM job_applications WHERE user_id = ?";
+$stmt_delete_applications = mysqli_prepare($conn, $sql_delete_applications);
+mysqli_stmt_bind_param($stmt_delete_applications, "i", $user_id);
+if (!mysqli_stmt_execute($stmt_delete_applications)) {
     die("Error executing query: " . mysqli_error($conn));
 } else {
-    $sql = "DELETE FROM users WHERE user_id = ?";
-    $stmt = mysqli_prepare($conn, $sql);
-    mysqli_stmt_bind_param($stmt, "i", $user_id);
-    if (!mysqli_stmt_execute($stmt)) {
+    // Delete job listings related to the user
+    $sql_delete_jobs = "DELETE FROM job_listing WHERE user_id = ?";
+    $stmt_delete_jobs = mysqli_prepare($conn, $sql_delete_jobs);
+    mysqli_stmt_bind_param($stmt_delete_jobs, "i", $user_id);
+    if (!mysqli_stmt_execute($stmt_delete_jobs)) {
         die("Error executing query: " . mysqli_error($conn));
+    } else {
+        $sql = "DELETE FROM users WHERE user_id = ?";
+        $stmt = mysqli_prepare($conn, $sql);
+        mysqli_stmt_bind_param($stmt, "i", $user_id);
+        if (!mysqli_stmt_execute($stmt)) {
+            die("Error executing query: " . mysqli_error($conn));
+        }
     }
 }
 ?>
